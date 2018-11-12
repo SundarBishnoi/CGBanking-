a9a5ff0b075fb2eeb003a17f11cbaf336a6b2b90
Sundar Bishnoi <sundarlal2929@gmail.com>
2018-11-12 08:17:25 +0530

    Add files via upload

---
+++ CgBankingSystem/src/com/cg/banking/daoservices/AccountDAOImpl.java
@@ -0,0 +1,132 @@
+package com.cg.banking.daoservices;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cg.banking.beans.Account;
+import com.cg.banking.beans.Transaction;
+import com.cg.banking.util.ConnectionProvider;
+
+public class AccountDAOImpl implements AccountDAO {
+	private Connection conn=ConnectionProvider.getDBConnection();
+	@Override
+	public long saveAccountOpen(String accountType, float initBalance) {
+		// TODO Auto-generated method stub
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt= conn.prepareStatement("INSERT INTO accountdetails(accountType,accountBalance,status,accountNo,pinNumber)values(?,?,?,account.nextval,?)");
+			pstmt.setString(1, accountType);
+			pstmt.setFloat(2,initBalance);
+			pstmt.setString(3, "Active");
+			pstmt.setInt(4,(int)(Math.random()*9000)+1000);
+			pstmt.executeUpdate();
+			
+			PreparedStatement pstmt2 = conn.prepareStatement("SELECT max(accountNo) from accountdetails");
+			ResultSet rs= pstmt2.executeQuery();
+			rs.next();
+			int accountNo=rs.getInt(1);
+			conn.commit();
+			return accountNo;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	@Override
+	public Account findAccount(long accountNo) {
+		// TODO Auto-generated method stub
+		try{
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt1= conn.prepareStatement("SELECT * from accountdetails where accountNo="+accountNo);
+			ResultSet rs= pstmt1.executeQuery();
+			if(rs.next()){
+				       String accountType = rs.getString("accountType");
+				       float accountBalance =rs.getFloat("accountBalance");
+				       int pinNumber = rs.getInt("pinNumber");
+				       String status =rs.getString("status");
+						Account account =  new Account(pinNumber, accountType, status, accountBalance, accountNo);
+						conn.commit();
+						return account;
+			}						
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean updateAccount(long accountNo, float accountBalance,String status,int pinNumber){
+		try{
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt1 = conn.prepareStatement("UPDATE accountdetails SET accountBalance=?,status=?,pinNumber=? where accountNo=?");
+			pstmt1.setFloat(1, accountBalance);
+			pstmt1.setString(2, status);
+			pstmt1.setInt(3, pinNumber);
+			pstmt1.setLong(4,accountNo);
+			pstmt1.executeUpdate();
+			conn.commit();
+			return true;
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	@Override
+	public Transaction saveTransaction(float amount, String transactionType, long accountNo) {
+		// TODO Auto-generated method stub
+		try{
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt1 = conn.prepareStatement("INSERT INTO Transactions(transactionId,amount,transactionType,accountNo) values(transactionsId.nextval,?,?,?)");
+			pstmt1.setFloat(1, amount);
+			pstmt1.setString(2, transactionType);
+			pstmt1.setLong(3, accountNo);
+			pstmt1.executeUpdate();
+			
+			PreparedStatement pstmt2 = conn.prepareStatement("SELECT max(transactionId) from Transactions");
+			ResultSet rs= pstmt2.executeQuery();
+			rs.next();
+			int transactionId = rs.getInt("transactionId");
+			
+			Transaction transaction = new Transaction(transactionId, amount, transactionType);
+			conn.commit();
+			return transaction;
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	@Override
+	public List<Account> findAllAccount() {
+		// TODO Auto-generated method stub
+		try{
+			conn.setAutoCommit(false);
+			PreparedStatement pstmt1= conn.prepareStatement("SELECT * from accountdetails");
+			ResultSet rs= pstmt1.executeQuery();
+			List<Account> accountList = new ArrayList<Account>();
+			while(rs.next()){
+				   String accountType = rs.getString("accountType");
+			       float accountBalance =rs.getFloat("accountBalance");
+			       int pinNumber = rs.getInt("pinNumber");
+			       String status =rs.getString("status"); 
+			       long accountNo = rs.getLong("accountNo");
+			       Account account = new Account(pinNumber, accountType, status, accountBalance, accountNo);
+			       accountList.add(account);
+			}						
+			return accountList;
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
---
+++ CgBankingSystem/src/com/cg/banking/services/BankingServicesImpl.java
@@ -0,0 +1,125 @@
+package com.cg.banking.services;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.cg.banking.beans.Account;
+import com.cg.banking.beans.Transaction;
+import com.cg.banking.daoservices.AccountDAO;
+import com.cg.banking.daoservices.AccountDAOImpl;
+import com.cg.banking.exceptions.AccountBlockedException;
+import com.cg.banking.exceptions.AccountNotFoundException;
+import com.cg.banking.exceptions.BankingServicesDownException;
+import com.cg.banking.exceptions.InsufficientAmountException;
+import com.cg.banking.exceptions.InvalidAccountTypeException;
+import com.cg.banking.exceptions.InvalidAmountException;
+import com.cg.banking.exceptions.InvalidPinNumberException;
+
+public class BankingServicesImpl implements BankingServices {
+	Account account = new Account();
+	AccountDAO accountDAO = new AccountDAOImpl();
+	Transaction transaction = new Transaction();
+	@Override
+	public long openAccount(String accountType, float initBalance)
+			throws InvalidAmountException, InvalidAccountTypeException, BankingServicesDownException {
+		// TODO Auto-generated method stub
+		long accountNo=accountDAO.saveAccountOpen(accountType, initBalance);
+		
+		return accountNo;
+	}
+	
+	@Override
+	public float depositAmount(long accountNo, float amount)
+			throws AccountNotFoundException, BankingServicesDownException, AccountBlockedException {
+		Account account = accountDAO.findAccount(accountNo);
+		
+		if(account!=null){
+			accountDAO.saveTransaction(amount, "Credit", accountNo);
+			float accountBalance= account.getAccountBalance();
+			String status = account.getStatus();
+			int pinNumber = account.getPinNumber();
+			accountBalance=accountBalance+amount;
+			account.setAccountBalance(accountBalance);
+			boolean t = accountDAO.updateAccount(accountNo, accountBalance, status, pinNumber);
+			
+			if(t) return accountBalance;
+		}
+		return 0;
+	}
+
+	@Override
+	public float withdrawAmount(long accountNo, float amount, int pinNumber) throws InsufficientAmountException,
+			AccountNotFoundException, InvalidPinNumberException, BankingServicesDownException, AccountBlockedException {
+		// TODO Auto-generated method stub
+		Account account = accountDAO.findAccount(accountNo);
+		if(account!=null) {
+			accountDAO.saveTransaction(amount, "Debit", accountNo);
+			float accountBalance = account.getAccountBalance();
+			String status = account.getStatus();
+			if(amount<=accountBalance) {
+				accountBalance = accountBalance - amount ;
+				account.setAccountBalance(accountBalance);
+				boolean t = accountDAO.updateAccount(accountNo, accountBalance, status, pinNumber);
+				if(t) return accountBalance;
+			}
+		}
+		return 0;
+	}
+
+	@Override
+	public boolean fundTransfer(long accountNoTo, long accountNoFrom, float transferAmount, int pinNumber)
+			throws InsufficientAmountException, AccountNotFoundException, InvalidPinNumberException,
+			BankingServicesDownException, AccountBlockedException {
+		// TODO Auto-generated method stub
+		Account account1 = accountDAO.findAccount(accountNoFrom);
+		Account account2 = accountDAO.findAccount(accountNoTo);
+		if((account1!=null)&&(account2!=null)) {
+			float account1Balance= account1.getAccountBalance();
+			float account2Balance= account2.getAccountBalance();
+			if(transferAmount<=account1Balance) {
+				account1Balance-=transferAmount;
+				account2Balance+=transferAmount;
+				String status1 =account1.getStatus();
+				String status2 = account2.getStatus();
+				int pinNumber2 = account2.getPinNumber();
+				boolean t1 = accountDAO.updateAccount(accountNoFrom, account1Balance, status1,pinNumber);
+				boolean t2 = accountDAO.updateAccount(accountNoTo, account2Balance, status2, pinNumber2);
+				if(t1 && t2) return true;
+			}
+			
+		}
+		return false;
+	}
+
+	@Override
+	public Account getAccountDetails(long accountNo) throws AccountNotFoundException, BankingServicesDownException {
+		// TODO Auto-generated method stub
+		Account account = accountDAO.findAccount(accountNo);
+		
+		return account;
+	}
+
+	@Override
+	public List<Account> getAllAccountDetails() throws BankingServicesDownException {
+		// TODO Auto-generated method stub
+		return accountDAO.findAllAccount();
+		
+	}
+
+	@Override
+	public List<Transaction> getAccountAllTransaction(long accountNo)
+			throws BankingServicesDownException, AccountNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String accountStatus(long accountNo)
+			throws BankingServicesDownException, AccountNotFoundException, AccountBlockedException {
+		// TODO Auto-generated method stub
+		Account account = accountDAO.findAccount(accountNo);
+		String status = account.getStatus();
+		return status;
+	}
+}
